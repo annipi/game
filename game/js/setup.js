769dbc6a2bd44c6c4578203c47809ef112dff3a9
Anni Piragauta <anni.apu@gmail.com>
2017-05-27 10:54:37 -0500

    first changes

---
+++ game/js/setup.js
@@ -0,0 +1,7 @@
+var game = new Phaser.Game(830, 380, Phaser.AUTO, 'flyGame');
+var fly;
+
+game.state.add('play', playState);
+//game.state.add('game_over', gameOverState);
+
+game.state.start('play');
